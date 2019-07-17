8885bddb6b166a85b0ffc86c962cb6716bbb7b54
Leon <lronssin@rootstack.com>
2019-07-17 15:41:23 -0500

    update icons and add spinners

--- resources/js/faIcons.js
+++ resources/js/faIcons.js
@@ -1,14 +1,16 @@
 // https://fortawesome.com/sets/font-awesome-5-solid
 import {
     faCalendarAlt,
+    faCheck,
     faDownload,
+    faEllipsisH,
     faPlusSquare,
-    faSpinner,
     faUserAlt
 } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 library.add(faCalendarAlt)
+library.add(faCheck)
 library.add(faDownload)
+library.add(faEllipsisH)
 library.add(faPlusSquare)
-library.add(faSpinner)
 library.add(faUserAlt)
