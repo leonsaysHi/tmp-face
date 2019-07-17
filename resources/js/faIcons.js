e55cf4907c07fad6b929511a4c5666b463d6af60
Leon <lronssin@rootstack.com>
2019-07-17 14:44:12 -0500

    spinner icon

--- resources/js/faIcons.js
+++ resources/js/faIcons.js
@@ -3,10 +3,12 @@ import {
     faCalendarAlt,
     faDownload,
     faPlusSquare,
+    faSpinner,
     faUserAlt
 } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 library.add(faCalendarAlt)
 library.add(faDownload)
 library.add(faPlusSquare)
+library.add(faSpinner)
 library.add(faUserAlt)
